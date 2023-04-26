7c8a46afa81dd2826f2e4fc1649a413e8a39d8ec
Andries16 <aaw2012sirbu.andrei@gmail.com>
2023-04-27 00:23:12 +0300

    login

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,19 @@
 $(window).on('load', function () {
   $('#loading').hide();
+
+  if(localStorage.getItem("user")){
+    document.getElementById("login").style.display = "none";
+    document.getElementById("login1").style.display = "none";
+  }else{
+    document.getElementById("logout").style.display = "none";
+    document.getElementById("logout1").style.display = "none";
+  }
+
+
+  $('#logout').click(()=>{
+    localStorage.removeItem("user");
+    window.location.href = "Login.html";
+  });
 }) 
+
+
